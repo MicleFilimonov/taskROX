6bb807123ab994c01240b7deadc1f38b5d16dde9
MicleFilimonov <115146590+MicleFilimonov@users.noreply.github.com>
2023-11-07 01:58:53 +0300

    Update then.js

--- test/steps/then.js
+++ test/steps/then.js
@@ -11,58 +11,58 @@ import news from '../../pageObject/news.js'
 import promotions from '../../pageObject/promotions.js'
 
 
-Then("Я проверяю статус New", async () => {
+Then(/^Я проверяю статус New$/, async () => {
     await statuses.confirmNew()
 })
 
-Then("Я проверяю, что почта подтверждена", async () => {
+Then(/^Я проверяю, что почта подтверждена$/, async () => {
     await statuses.confirmaEmail()
 })
 
-Then("Я вижу окно кассы", async () => {
+Then(/^Я вижу окно кассы$/, async () => {
     await cashForm.cashDisplayed()
 })
 
-Then("Я проверяю статус ELITE", async () => {
+Then(/^Я проверяю статус ELITE$/, async () => {
     await elite.checkElite()
 })
 
-Then("Я проверяю наличие турнира ROXSTARS", async () => {
+Then(/^Я проверяю наличие турнира ROXSTARS$/, async () => {
     await roxstars.roxstarsTournament()
 })
 
-Then("Я проверяю наличие подарка на ДР", async () => {
+Then(/^Я проверяю наличие подарка на ДР$/, async () => {
     await birthday.birthdayText()
 })
 
-Then("Я проверяю наличие кнопки Узнать больше", async () => {
+Then(/^Я проверяю наличие кнопки Узнать больше$/, async () => {
     await morebutton.getMoreButton()
 })
 
-Then("Я проверяю наличие блока Как получить кешбэк?", async () => {
+Then(/^Я проверяю наличие блока Как получить кешбэк$/, async () => {
     await cashback.titleCheck()
     await cashback.blockCheck()
 })
 
-Then("Я проверяю наличие 2-х любых новостей", async () => {
+Then(/^Я проверяю наличие 2-х любых новостей$/, async () => {
     await news.firstNewsCheck()
     await news.secondNewsCheck()
 })
 
-Then("Я проверяю наличие 2-х любых активных акций", async () => {
+Then(/^Я проверяю наличие 2-х любых активных акций$/, async () => {
     await promotions.firstNewsCheck()
     await promotions.secondNewsCheck()
 })
 
-Then("Я проверяю наличие 2-х любых завершенных акций", async () => {
+Then(/^Я проверяю наличие 2-х любых завершенных акций$/, async () => {
     await promotions.firstEndedNewsCheck()
     await promotions.secondEndedNewsCheck()
 })
 
-Then("Я вижу выпавший дропдаун", async () => {
+Then(/^Я вижу выпавший дропдаун$/, async () => {
     await promo.waitPromoButton()
 })
 
-Then("Я вижу окно кассы", async () => {
+Then(/^Я вижу окно кассы$/, async () => {
     cashForm.cashDisplayed()
 })
